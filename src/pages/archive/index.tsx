88870f7b49936763dbad174462463a3414cecf8d
Jan <kupper.thorge@gmail.com>
2025-08-18 14:29:02 +0200

    made archive responsive for mobile view

--- src/pages/archive/index.tsx
+++ src/pages/archive/index.tsx
@@ -5,15 +5,24 @@ import { useEffect, useState } from "react";
 
 export default function Index() {
   const { stories } = storiesData;
-  const [imageScale, setImageScale] = useState(1);
+  const [imageScale, setImageScale] = useState(0);
   const [hoveredStory, setHoveredStory] = useState<number | null>(null);
 
   const spans = {
-    index: [3, 6, 1],
-    title: [13, 10, 7],
-    genre: [12, 13, 9],
-    date: [1, 7, 3],
-    image: [8, 2, 13],
+    desktop: {
+      index: [3, 6, 1],
+      title: [13, 10, 7],
+      genre: [12, 13, 9],
+      date: [1, 7, 3],
+      image: [8, 2, 13],
+    },
+    mobile: {
+      index: [1, 2, 1],
+      title: [3, 3, 2],
+      genre: [3, 3, 3],
+      date: [1, 2, 1],
+      image: [2, 1, 3],
+    },
   };
 
   useEffect(() => {
@@ -42,7 +51,7 @@ export default function Index() {
 
       {/* List  */}
       {stories.map((story, index) => {
-        const i = index % spans.index.length;
+        const i = index % spans.desktop.index.length;
         const isHovered = hoveredStory === story.id;
 
         return (
@@ -50,34 +59,43 @@ export default function Index() {
             href={`/story/${story.title}`}
             key={story.id}
             style={{ translate: `0px -${i * 2}px` }}
-            className="border-dark hover:border-light border-y-2 py-8 transition-colors hover:z-[1]"
+            className="border-dark hover:border-light border-y-2 py-5 transition-colors hover:z-[1] sm:py-8"
           >
             <div
               onMouseEnter={() => setHoveredStory(story.id)}
               onMouseLeave={() => setHoveredStory(null)}
-              className="group grid h-32 grid-cols-[repeat(16,1fr)] grid-rows-[repeat(2,1fr)] gap-3"
+              style={
+                {
+                  translate: `0px -${i * 2}px`,
+                  "--desktop-index": spans.desktop.index[i],
+                  "--mobile-index": spans.mobile.index[i],
+                  "--desktop-title": spans.desktop.title[i],
+                  "--mobile-title": spans.mobile.title[i],
+                  "--desktop-genre": spans.desktop.genre[i],
+                  "--mobile-genre": spans.mobile.genre[i],
+                  "--desktop-date": spans.desktop.date[i],
+                  "--mobile-date": spans.mobile.date[i],
+                  "--desktop-image": spans.desktop.image[i],
+                  "--mobile-image": spans.mobile.image[i],
+                } as React.CSSProperties
+              }
+              className="group grid h-[calc(100vw/3-25px)] grid-cols-[repeat(3,1fr)] grid-rows-[repeat(2,1fr)] gap-3 sm:h-32 sm:grid-cols-[repeat(16,1fr)]"
             >
               {/* Index  */}
-              <div
-                className="relative col-span-3 h-fit w-full overflow-hidden"
-                style={{ gridColumnStart: spans.index[i] }}
-              >
+              <div className="relative col-span-1 [grid-column-start:var(--mobile-index)] h-fit w-full overflow-hidden sm:col-span-3 sm:[grid-column-start:var(--desktop-index)]">
                 {/* 1 */}
-                <p className="archive-text truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   0{story.id}
                 </p>
                 {/* 2 */}
-                <p className="archive-text absolute top-full truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text absolute top-full truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   Title: {story.displayName}
                 </p>
               </div>
               {/* Image  */}
               <div
-                className="col-span-4 flex items-center justify-center"
-                style={{
-                  gridColumnStart: spans.image[i],
-                  gridRow: "1 / 3",
-                }}
+                className="col-span-1 [grid-column-start:var(--mobile-image)] flex items-center justify-center sm:col-span-4 sm:[grid-column-start:var(--desktop-image)]"
+                style={{ gridRow: "1 / 3" }}
               >
                 <div className="relative aspect-auto h-full w-full overflow-hidden">
                   <div className="absolute top-1/2 left-1/2 aspect-square w-full -translate-x-1/2 -translate-y-1/2">
@@ -86,7 +104,7 @@ export default function Index() {
                         transform: `scale(${isHovered ? 1 : imageScale})`,
                         transition: "transform 400ms ease-out",
                       }}
-                      className="h-full w-full object-cover transition-transform duration-400 ease-out group-hover:scale-100"
+                      className="h-full w-full object-cover"
                       height={1024}
                       width={1024}
                       src={`/stories/${story.title}/${story.images[0].src}`}
@@ -96,44 +114,38 @@ export default function Index() {
                 </div>
               </div>
               {/* Title  */}
-              <div
-                style={{ gridColumnStart: spans.title[i] }}
-                className="relative col-span-3 h-fit w-full overflow-hidden"
-              >
+              <div className="relative col-span-2 [grid-column-start:var(--mobile-title)] h-fit w-full overflow-hidden sm:col-span-3 sm:[grid-column-start:var(--desktop-title)]">
                 {/* 1 */}
-                <p className="archive-text truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   Title: {story.displayName}
                 </p>
                 {/* 2 */}
-                <p className="archive-text absolute top-full truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text absolute top-full truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   Genre: {story.genre}
                 </p>
               </div>
               {/* Genre  */}
               <div
-                style={{ gridColumnStart: spans.genre[i], gridRowStart: 2 }}
-                className="relative col-span-3 h-fit w-full self-end overflow-hidden"
+                className="relative col-span-1 [grid-column-start:var(--mobile-genre)] h-fit w-full self-end overflow-hidden sm:col-span-3 sm:[grid-column-start:var(--desktop-genre)]"
+                style={{ gridRowStart: 2 }}
               >
                 {/* 1 */}
-                <p className="archive-text truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   Genre: {story.genre}
                 </p>
                 {/* 2 */}
-                <p className="archive-text absolute top-full truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text absolute top-full truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   {story.screeningDate}
                 </p>
               </div>
               {/* Date  */}
-              <div
-                style={{ gridColumnStart: spans.date[i] }}
-                className="relative col-span-3 h-fit w-full self-end overflow-hidden"
-              >
+              <div className="relative col-span-1 [grid-column-start:var(--mobile-date)] h-fit w-full self-end overflow-hidden sm:col-span-3 sm:[grid-column-start:var(--desktop-date)]">
                 {/* 1 */}
-                <p className="archive-text truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   {story.screeningDate}
                 </p>
                 {/* 2 */}
-                <p className="archive-text absolute top-full truncate transition-transform ease-out group-hover:-translate-y-full">
+                <p className="archive-text absolute top-full truncate transition-transform ease-out sm:group-hover:-translate-y-full">
                   0{story.id}
                 </p>
               </div>
