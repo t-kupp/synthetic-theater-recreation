79a55ae691f92a4220fdd7aefc3926910c4354f8
Jan <kupper.thorge@gmail.com>
2025-08-19 16:07:47 +0200

    Added marquee text to manifesto

---
+++ src/components/manifesto/AnimatedText.tsx
@@ -0,0 +1,44 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+interface AnimatedTextProps {
+  text: string;
+  direction: "left" | "right";
+  leftPercent: number;
+}
+
+export default function AnimatedText({ text, direction, leftPercent }: AnimatedTextProps) {
+  const textRef = useRef<HTMLParagraphElement>(null);
+
+  useGSAP(() => {
+    const duration = 50 + Math.random() * 100;
+    console.log("duration:", duration);
+
+    if (direction === "right") {
+      gsap.to(textRef.current, { xPercent: 100, duration: duration, ease: "none", repeat: -1 });
+    }
+
+    if (direction === "left") {
+      gsap.to(textRef.current, { xPercent: -100, duration: duration, ease: "none", repeat: -1 });
+    }
+  });
+
+  return (
+    <div
+      ref={textRef}
+      style={{ left: `${leftPercent}%` }}
+      className={"font-bit relative w-full text-[4rem] leading-[0.6] md:text-[11vh]"}
+    >
+      <p className="w-full uppercase">{text}</p>
+      <p
+        className={
+          (direction === "right" ? "right-[100%]" : "left-[100%]") +
+          " absolute top-0 w-full uppercase"
+        }
+      >
+        {text}
+      </p>
+    </div>
+  );
+}
--- src/pages/manifesto/index.tsx
+++ src/pages/manifesto/index.tsx
@@ -1,37 +1,58 @@
+import ShapeBackground from "@/components/header/ShapeBackground";
+import AnimatedText from "@/components/manifesto/AnimatedText";
 import TextSection from "@/components/manifesto/TextSection";
 
 export default function Index() {
   return (
-    <section className="pt-header-height flex flex-col gap-72 px-5">
-      <TextSection index={1}>
-        This website is a recreation of{" "}
-        <a
-          target="_blank"
-          className="text-light border-b transition-colors hover:border-transparent"
-          href="https://www.designisfunny.co/"
-        >{`Daniele Buffa's`}</a>{" "}
-        wonderful passion project{" "}
-        <a
-          target="_blank"
-          className="text-light border-b transition-colors hover:border-transparent"
-          href="https://www.synthetictheatre.com/"
-        >
-          Synthetic Theatre
-        </a>
-        . "It is a realm where stories unravel, and where visual freedom is at the core."
-      </TextSection>
-      <TextSection index={2} reversed>
-        I chose this website to recreate because, as a passion project, it is free from conventional
-        limitations like client constraints or budget limits. Pure creative freedom.
-      </TextSection>
-      <TextSection index={3}>
-        This recreation is not perfect. But "Make it work with my current skills" forced creative
-        solutions I wouldn't have discovered otherwise.
-      </TextSection>
-      <TextSection index={4} reversed>
-        hi
-      </TextSection>
-      <TextSection index={5}>hi</TextSection>
-    </section>
+    <>
+      <section className="relative h-[100svh]">
+        <div className="pt-header-height absolute inset-0 z-[1] flex flex-col gap-y-4 overflow-hidden md:mt-16 md:gap-y-1">
+          <AnimatedText leftPercent={5} text="If I'm to" direction="right" />
+          <AnimatedText leftPercent={15} text="Disappear" direction="left" />
+          <AnimatedText leftPercent={50} text="So would" direction="left" />
+          <AnimatedText leftPercent={35} text="You" direction="right" />
+          <AnimatedText leftPercent={3} text="But in your" direction="left" />
+          <AnimatedText leftPercent={50} text="Absence" direction="right" />
+          <AnimatedText leftPercent={2} text="I would" direction="left" />
+          <AnimatedText leftPercent={30} text="Continue" direction="left" />
+          <AnimatedText leftPercent={65} text="To exist" direction="left" />
+        </div>
+        <div className="absolute inset-0">
+          <ShapeBackground />
+          <div className="bg-background/80 absolute bottom-6 h-12 w-full"></div>
+        </div>
+      </section>
+      <section className="flex flex-col gap-72 px-5">
+        <TextSection index={1}>
+          This website is a recreation of{" "}
+          <a
+            target="_blank"
+            className="text-light border-b transition-colors hover:border-transparent"
+            href="https://www.designisfunny.co/"
+          >{`Daniele Buffa's`}</a>{" "}
+          wonderful passion project{" "}
+          <a
+            target="_blank"
+            className="text-light border-b transition-colors hover:border-transparent"
+            href="https://www.synthetictheatre.com/"
+          >
+            Synthetic Theatre
+          </a>
+          . "It is a realm where stories unravel, and where visual freedom is at the core."
+        </TextSection>
+        <TextSection index={2} reversed>
+          I chose this website to recreate because, as a passion project, it is free from
+          conventional limitations like client constraints or budget limits. Pure creative freedom.
+        </TextSection>
+        <TextSection index={3}>
+          This recreation is not perfect. But "Make it work with my current skills" forced creative
+          solutions I wouldn't have discovered otherwise.
+        </TextSection>
+        <TextSection index={4} reversed>
+          hi
+        </TextSection>
+        <TextSection index={5}>hi</TextSection>
+      </section>
+    </>
   );
 }
